c1eb0f9adac040ebaacd68fc2a4e958c29da1dc3
Byte Alchemist <byte8alchemist@gmail.com>
2026-03-05 14:54:41 -0500

    Add ScrollToTop component to App for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Services from "./pages/Services";
 import Team from "./pages/Team";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
